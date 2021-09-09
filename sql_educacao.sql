3ea20e6081c362a51bc3a6af5851082031fcde14
Jonas A. Dhein <jonasdhein@gmail.com>
2021-09-08 21:29:13 -0300

    Update sql_educacao.sql

--- sql_educacao.sql
+++ sql_educacao.sql
@@ -1,19 +1,12 @@
+--LISTAR idade DO CANDIDATO
 SELECT id, DATE_PART('year', age(data_nascimento)) as idade, 
 data_nascimento 
 FROM candidatos
 ORDER BY DATE_PART('year', age(data_nascimento));
 
 --LISTAR O NIVEL DE ENSINO DO CANDIDATO
-SELECT
-FROM candidatos c, ...
-
---LISTAR A TURMA DA ESCOLA DE ACORCO COM A FAIXA ETÁRIA DO CANDIDATO
-SELECT 
-FROM candidatos c, 
-
-
-
-SELECT * FROM candidatos_escolas
-
-UPDATE candidatos_escolas 
-SET id_turma = ???;
+SELECT c.id, c.data_nascimento, DATE_PART('year', age(data_nascimento)) as idade,
+n.nome, n.idade_inicio, n.idade_fim
+FROM candidatos c, niveis_ensino n
+WHERE DATE_PART('year', age(data_nascimento)) >= idade_inicio 
+AND DATE_PART('year', age(data_nascimento)) < idade_fim
